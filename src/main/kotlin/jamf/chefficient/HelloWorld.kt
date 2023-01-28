b9e4ec12ea4f68619cf875b496ad16ecd31e887a
Jose Martin <j.antonio.martin.fdez@gmail.com>
2023-01-28 20:16:10 +0100

    Initial commit

---
+++ src/main/kotlin/jamf/chefficient/HelloWorld.kt
@@ -0,0 +1,9 @@
+package jamf.chefficient
+
+import io.javalin.Javalin
+
+fun main() {
+    Javalin.create(/*config*/)
+        .get("/") { ctx -> ctx.result("Hello World") }
+        .start(7070)
+}
