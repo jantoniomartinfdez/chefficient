6f99a48e36cb6e089d3b8cbe060953b6b9e69492
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-10-15 21:23:09 +0200

    Documenting test class for further test cases

--- src/test/kotlin/jamf/chefficient/domain/food/FoodTest.kt
+++ src/test/kotlin/jamf/chefficient/domain/food/FoodTest.kt
@@ -3,6 +3,25 @@ package jamf.chefficient.domain.food
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+/**
+ * Business rules:
+ *     - It should have a non-empty name
+ *     - It should have a numbered quantity
+ *     - It should have an existing measurable unit
+ *     - It should not have a sell-by date if it is not perishable
+ *     - It should have a sell-by date if it is perishable
+ * Invariants:
+ *     - It should not change its name
+ *     - It should not change its quantity
+ *     - It should not change its unit
+ *     - It should not change its batch date
+ *     - It should not change its sell-by date
+ * Examples:
+ *     - [PERISHABLE][INDUSTRIAL FOOD] Yogurt: 4 units of yogurt, batch date 01-01-2024, sell-by date 01-02-2024
+ *     - [PERISHABLE][HOMEMADE FOOD] Bread: a loaf of bread, batch date NULL, sell-by date +1 week (default one when no specified but needed)
+ *     - [NON-PERISHABLE][HOMEMADE FOOD] Honey: a liter of honey, batch date NULL, sell-by date NULL
+ *     - [NON-PERISHABLE][INDUSTRIAL FOOD] Salt: 1 kilo of salt in bulk, batch date 01-01-2024, sell-by date NULL
+ */
 internal class FoodTest {
 
     @Test
