edfc148aefb855bb3b3b28ed658fad6894773512
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-11-04 17:06:41 +0100

    RecipeTest::should not allow a blank title => REFACTOR

--- src/main/kotlin/jamf/chefficient/domain/recipe/Recipe.kt
+++ src/main/kotlin/jamf/chefficient/domain/recipe/Recipe.kt
@@ -3,10 +3,6 @@ package jamf.chefficient.domain.recipe
 class Recipe private constructor(val title: String) {
     companion object {
         fun create(title: String): Recipe {
-            if (title.isEmpty()) {
-                throw MissingTitle("Recipes must have an alphanumeric title")
-            }
-
             if (title.isBlank()) {
                 throw MissingTitle("Recipes must have an alphanumeric title")
             }
