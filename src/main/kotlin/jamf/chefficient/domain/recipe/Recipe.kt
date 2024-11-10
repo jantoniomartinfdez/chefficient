b59bc2ed5539f5004cf7cfe96bb93b0e047116a0
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-11-10 12:40:51 +0100

    RecipeTest::may have a recommendation => GREEN

--- src/main/kotlin/jamf/chefficient/domain/recipe/Recipe.kt
+++ src/main/kotlin/jamf/chefficient/domain/recipe/Recipe.kt
@@ -1,13 +1,13 @@
 package jamf.chefficient.domain.recipe
 
-class Recipe private constructor(val title: String, val description: String) {
+class Recipe private constructor(val title: String, val description: String, val recommendation: String) {
     companion object {
         fun create(title: String, description: String = "", recommendation: String = ""): Recipe {
             if (title.isBlank()) {
                 throw MissingTitle("Recipes must have an alphanumeric title")
             }
 
-            return Recipe(title, description)
+            return Recipe(title, description, recommendation)
         }
     }
 }
