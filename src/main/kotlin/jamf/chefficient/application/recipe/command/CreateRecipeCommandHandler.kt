4aba7d4ada3eb6068d71125fb5d5f3a0646064a0
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-11-26 00:05:59 +0100

    Given a recipe titled 'French Omelette' does not exist, when creating it, then it should be saved => GREEN

--- src/main/kotlin/jamf/chefficient/application/recipe/command/CreateRecipeCommandHandler.kt
+++ src/main/kotlin/jamf/chefficient/application/recipe/command/CreateRecipeCommandHandler.kt
@@ -1,6 +1,7 @@
 package jamf.chefficient.application.recipe.command
 
 import jamf.chefficient.application.recipe.exception.RecipeAlreadyExists
+import jamf.chefficient.domain.recipe.Recipe
 import jamf.chefficient.domain.recipe.RecipeRepository
 
 class CreateRecipeCommandHandler(private val recipeRepository: RecipeRepository) {
@@ -8,5 +9,14 @@ class CreateRecipeCommandHandler(private val recipeRepository: RecipeRepository)
         if (recipeRepository.contains(command.title)) {
             throw RecipeAlreadyExists("The recipe '${command.title}' already exists!")
         }
+
+        val recipe = Recipe.create(
+            command.title,
+            command.ingredients,
+            command.steps,
+            command.description,
+            command.recommendation
+        )
+        recipeRepository.save(recipe)
     }
 }
