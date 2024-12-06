c03f24b89b14238105a23c64ee724c7d333daba1
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-12-06 15:37:56 +0100

    DbConnectionProviderIntegrationTest::should release system resources after using the DB configuration file => REFACTOR

--- src/main/kotlin/jamf/chefficient/infrastructure/persistence/DbConnectionProviderFactory.kt
+++ src/main/kotlin/jamf/chefficient/infrastructure/persistence/DbConnectionProviderFactory.kt
@@ -10,22 +10,26 @@ open class DbConnectionProviderFactory {
             ?: throw FileNotFoundException("The file application.properties does not exist!")
 
         try {
-            val properties = Properties()
-            properties.load(inputStream)
-            val dbUrl = properties.getProperty("db.url")
-            val dbUser = properties.getProperty("db.username")
-            val dbPassword = properties.getProperty("db.password")
-
-            if (dbUrl == null || dbUser == null || dbPassword == null) {
-                throw DbCredentialsNotFound("DB credentials within the file application.properties don't exist!")
-            }
-
-            return DbConnectionProvider(dbUrl, dbUser, dbPassword)
+            return createInstance(inputStream)
         } catch (exception: Throwable) {
             throw exception
         } finally {
             releaseSystemResources(inputStream)
         }
+    }
+
+    private fun createInstance(inputStream: InputStream): DbConnectionProvider {
+        val properties = Properties()
+        properties.load(inputStream)
+        val dbUrl = properties.getProperty("db.url")
+        val dbUser = properties.getProperty("db.username")
+        val dbPassword = properties.getProperty("db.password")
+
+        if (dbUrl == null || dbUser == null || dbPassword == null) {
+            throw DbCredentialsNotFound("DB credentials within the file application.properties don't exist!")
+        }
+
+        return DbConnectionProvider(dbUrl, dbUser, dbPassword)
     }
 
     protected open fun releaseSystemResources(inputStream: InputStream) {
