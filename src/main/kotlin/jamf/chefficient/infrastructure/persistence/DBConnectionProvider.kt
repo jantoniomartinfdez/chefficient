d307752ed608a95f2a2e86ce927ec538599d2a50
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-12-06 11:55:41 +0100

    DBConnectionProviderIntegrationTest::Given a DB configuration file exists, and doesn't contain DB credentials, when creating the connection from it, then it should fail => GREEN

--- src/main/kotlin/jamf/chefficient/infrastructure/persistence/DBConnectionProvider.kt
+++ src/main/kotlin/jamf/chefficient/infrastructure/persistence/DBConnectionProvider.kt
@@ -3,6 +3,7 @@ package jamf.chefficient.infrastructure.persistence
 import java.io.FileNotFoundException
 import java.sql.Connection
 import java.sql.DriverManager
+import java.util.*
 
 class DBConnectionProvider(
     private val url: String,
@@ -22,6 +23,16 @@ class DBConnectionProvider(
         fun fromConfiguration() {
             val inputStream = this::class.java.classLoader.getResourceAsStream("application.properties")
                 ?: throw FileNotFoundException("The file application.properties does not exist!")
+
+            val properties = Properties()
+            properties.load(inputStream)
+            val dbUrl = properties.getProperty("db.url")
+            val dbUser = properties.getProperty("db.username")
+            val dbPassword = properties.getProperty("db.password")
+
+            if (dbUrl == null || dbUser == null || dbPassword == null) {
+                throw DbCredentialsNotFound("DB credentials within the file application.properties don't exist!")
+            }
         }
     }
 }
