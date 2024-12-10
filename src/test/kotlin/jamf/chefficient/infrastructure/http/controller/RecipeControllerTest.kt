abe93029b82e6a2598c4dd1105a0aa100378c6fa
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-12-10 18:23:08 +0100

    RecipeControllerTest::Given I have an invalid recipe request, when I try to create it, then it should respond Bad-Request => RED

--- src/test/kotlin/jamf/chefficient/infrastructure/http/controller/RecipeControllerTest.kt
+++ src/test/kotlin/jamf/chefficient/infrastructure/http/controller/RecipeControllerTest.kt
@@ -2,9 +2,11 @@ package jamf.chefficient.infrastructure.http.controller
 
 import io.javalin.http.Context
 import io.javalin.http.HttpStatus
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import jamf.chefficient.application.recipe.command.CreateRecipeCommandHandler
+import jamf.chefficient.domain.recipe.MissingTitle
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -22,6 +24,24 @@ class RecipeControllerTest {
 
     @Test
     fun `Given I have a malformed or empty recipe request, when I try to create it, then it should respond Bad-Request`() {
+        systemUnderTest.create.handle(contextStub)
+
+        verify { contextStub.status(eq(HttpStatus.BAD_REQUEST.code)) }
+    }
+
+    @Test
+    fun `Given I have an invalid recipe request, when I try to create it, then it should respond Bad-Request`() {
+        every { contextStub.body() } returns """
+            {
+                "title": "",
+                "ingredients": [],
+                "steps": []
+                "description": "",
+                "recommendation": ""
+            }
+        """.trimIndent()
+        every { createRecipeCommandHandlerStub.handle(any()) } throws MissingTitle("Exception message stub")
+
         systemUnderTest.create.handle(contextStub)
 
         verify { contextStub.status(eq(HttpStatus.BAD_REQUEST.code)) }
