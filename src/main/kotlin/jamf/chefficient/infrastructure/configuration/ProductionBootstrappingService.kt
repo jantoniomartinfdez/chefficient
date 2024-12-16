09a96e30a140d6fff40d659c632f225fd051d898
Jose Martin <j.antonio.martin.fdez@gmail.com>
2024-12-16 11:55:55 +0100

    Chefficient::Creating recipes with dev/prod configuration should work => RED + GREEN

--- src/main/kotlin/jamf/chefficient/infrastructure/configuration/ProductionBootstrappingService.kt
+++ src/main/kotlin/jamf/chefficient/infrastructure/configuration/ProductionBootstrappingService.kt
@@ -1,6 +1,10 @@
 package jamf.chefficient.infrastructure.configuration
 
+import jamf.chefficient.application.recipe.command.CreateRecipeCommandHandler
+import jamf.chefficient.infrastructure.http.controller.RecipeController
+import jamf.chefficient.infrastructure.persistence.DbConnectionProvider
 import jamf.chefficient.infrastructure.persistence.DbConnectionProviderFactory
+import jamf.chefficient.infrastructure.persistence.postgresql.RecipeRepository
 import org.flywaydb.core.Flyway
 
 object ProductionBootstrappingService: BootstrappingService {
@@ -13,6 +17,20 @@ object ProductionBootstrappingService: BootstrappingService {
             .locations("filesystem:src/main/resources/flyway/migrations")
             .load()
         flyway.migrate()
+
+        ServiceLocator.registerService(DbConnectionProvider::class.qualifiedName!!, dbConnectionProvider)
+
+        val recipeRepository = RecipeRepository(dbConnectionProvider)
+        ServiceLocator.registerService(jamf.chefficient.domain.recipe.RecipeRepository::class.qualifiedName!!, recipeRepository)
+
+        val createRecipeCommandHandler = CreateRecipeCommandHandler(recipeRepository)
+        ServiceLocator.registerService(
+            CreateRecipeCommandHandler::class.qualifiedName!!,
+            createRecipeCommandHandler
+        )
+
+        val recipeController = RecipeController(createRecipeCommandHandler)
+        ServiceLocator.registerService(RecipeController::class.qualifiedName!!, recipeController)
     }
 
     override fun javalinPort(): Int {
